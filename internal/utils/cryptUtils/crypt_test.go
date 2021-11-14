81d8888c82870cd856b0496565478a82246fd822
aghost <ggg17226@gmail.com>
2021-11-14 15:24:32 +0800

    feat: move test file to source package

--- internal/utils/cryptUtils/crypt_test.go
+++ internal/utils/cryptUtils/crypt_test.go
@@ -8,12 +8,11 @@
 package cryptUtils
 
 import (
-	"github.com/AghostPrj/vm-manager-backend/internal/utils/cryptUtils"
 	"testing"
 )
 
 func TestHmacSha256(t *testing.T) {
-	sha256 := cryptUtils.HmacSha256("aaaaa", "aa")
+	sha256 := HmacSha256("aaaaa", "aa")
 	if sha256 != "574b1d75c67447d55da4b88ba283d866ddb267143575778d15c936993e69c98d" {
 		t.Fatalf("calc hmac sha256 error")
 	}
